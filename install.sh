b2ec6913d200271d3ebd770812a82946cef462d4
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2014-05-21 15:55:58 -0700

    zsh syntax highlighting

--- install.sh
+++ install.sh
@@ -10,10 +10,14 @@ git submodule update --init
 
 cd $ROOT
 
-ln -sf `pwd`/configs/* $HOME/
+for f in `ls -a configs | grep -E '\.\w+'`; do
+  ln -sf `pwd`/configs/$f $HOME/$f;
+done
 
 ln -sf `pwd`/ssh/config $HOME/.ssh/config
 
 ln -sf `pwd`/bundle/tmux/.tmux.conf $HOME/.tmux.conf
 ln -sf `pwd`/bundle/vim/vimrc $HOME/.vimrc
 ln -sf `pwd`/bundle/oh-my-zsh $HOME/.oh-my-zsh
+
+ln -sf `pwd`/bundle/zsh/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/
