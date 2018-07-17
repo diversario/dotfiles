91e95b973646d6ce8d6364c87275fe64fef67e83
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2018-07-16 18:37:36 -0700

    Add kubectl alias generator and bin getter

---
+++ get-binaries.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#set -x
+
+OS=$(uname -a | cut -d' ' -f1 | awk '{print tolower($0)}')
+
+HELM_URL="https://storage.googleapis.com/kubernetes-helm/"
+HELM_TAR="helm-vVERSION-$OS-amd64.tar.gz"
+KUBECTL_URL="https://storage.googleapis.com/kubernetes-release/release/vVERSION/bin/$OS/amd64/kubectl"
+
+mkdir -p ~/bin
+
+for v in '2.2.3' '2.7.2' '2.9.1'; do
+  tar_filename=/tmp/helmv$v.tar.gz
+  filter=$OS-amd64/helm
+  bin_name=~/bin/helm$(echo $v | sed -e 's/\.//g')
+  url=$HELM_URL$(echo $HELM_TAR | sed -e "s/VERSION/$v/")
+
+  echo "Downloading Helm v$v from $url to $bin_name"
+
+  curl -sL $url -o $tar_filename
+  tar xf $tar_filename --strip-components=1 -C /tmp $filter
+  mv /tmp/helm $bin_name
+  chmod +x $bin_name
+done
+
+
+for v in '1.5.3' '1.8.4' '1.9.2'; do
+  filename=~/bin/kubectl$(echo $v | sed -e 's/\.//g')
+  url=$(echo $KUBECTL_URL | sed -e "s/VERSION/$v/")
+  echo "Downloading kubectl v$v from $url to $filename"
+  curl -sL $url -o $filename
+  chmod +x $filename
+done
+
+
+
